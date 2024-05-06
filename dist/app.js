bb7f730c69c4f9ed40a00997e68e78f2f940c10c
Nathan <npieraut@live.fr>
2024-05-05 23:03:48 -0400

    clean

--- dist/app.js
+++ dist/app.js
@@ -3,84 +3,72 @@ import path from 'path';
 import { readFileSync, writeFileSync } from 'fs';
 import express from 'express';
 const app = express();
-app.use(express.json()); 
-
+app.use(express.json());
 app.get('/add-request', (req, res) => {
-      processResources(projectPath).then((resources:any) => {
+    processResources(projectPath).then((resources) => {
         console.log('All Resources:', JSON.stringify(resources, null, 2));
-        buildRestCall('GET', resources[0].name, { description: 'one' }, resources[0].files[1])
-        });
-  res.status(200).json({ message: 'received' });
+        buildRestCall('GET', resources[0].name, { description: 'one' }, resources[0].files[1]);
+    });
+    res.status(200).json({ message: 'received' });
 });
-
-
 app.post('/create-new-resource/:id', (req, res) => {
     const id = req.params.id;
-    console.log(req.body)
+    console.log(req.body);
     createNewResource(projectPath, id, req.body.data);
     res.status(200).json({ message: 'received' });
-}
-);
+});
 app.post('/has-many', (req, res) => {
-    console.log('ss')
-    console.log(req.body)
+    console.log('ss');
+    console.log(req.body);
     hasMany(projectPath, req.body.data);
     res.status(200).json({ message: 'received' });
-}
-);
-
+});
 const PORT = process.env.PORT || 1234;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-async function getAllFiles(dirPath:any, arrayOfFiles : any[]= []) {
+async function getAllFiles(dirPath, arrayOfFiles = []) {
     const entries = await fs.readdir(dirPath, { withFileTypes: true });
     for (let entry of entries) {
         const fullPath = path.join(dirPath, entry.name);
         if (entry.isDirectory()) {
             arrayOfFiles = await getAllFiles(fullPath, arrayOfFiles);
-        } else {
+        }
+        else {
             arrayOfFiles.push(fullPath);
         }
     }
     return arrayOfFiles;
 }
-
-async function processResources(projectPath:any) {
+async function processResources(projectPath) {
     try {
         const files = await getAllFiles(projectPath);
         const cleanedFiles = files.map((file) => file.replace(projectPath, ''));
         const uniqueFolders = [...new Set(cleanedFiles
-            .filter(file => file.startsWith('/src/main/java/com/natetrystuff/'))
-            .map(file => {
+                .filter(file => file.startsWith('/src/main/java/com/natetrystuff/'))
+                .map(file => {
                 const match = file.match(/^\/src\/main\/java\/com\/natetrystuff\/([^\/]+)(\/|$)/);
                 return match ? match[1] : null;
-            })
-
-        )].filter(Boolean).filter((value)=>{
-        const isFile = value.includes('.');
-        if(isFile){
-            return false;
-        }
-        return true;
-    });
-
-        const resources = await Promise.all(uniqueFolders.map(async folder => {
+            }))].filter(Boolean).filter((value) => {
+            const isFile = value.includes('.');
+            if (isFile) {
+                return false;
+            }
+            return true;
+        });
+        const resources = await Promise.all(uniqueFolders.map(async (folder) => {
+            var _a;
             const resourceFiles = cleanedFiles.filter(file => file.startsWith(`/src/main/java/com/natetrystuff/${folder}/`));
             const controllerFile = resourceFiles.find(file => file.endsWith('Controller.java'));
-
-            let restMethods :any =[];
+            let restMethods = [];
             if (controllerFile) {
                 const data = await fs.readFile(projectPath + controllerFile, 'utf8');
-                restMethods = data?.match(/@[A-Za-z]+Mapping(\("[^"]*"\))?/g)?.filter(restCall => {
-                    if(restCall.includes('RequestMapping')){
-                        console.log('removing', restCall)
+                restMethods = (_a = data === null || data === void 0 ? void 0 : data.match(/@[A-Za-z]+Mapping(\("[^"]*"\))?/g)) === null || _a === void 0 ? void 0 : _a.filter(restCall => {
+                    if (restCall.includes('RequestMapping')) {
+                        console.log('removing', restCall);
                         return false;
                     }
                     return true;
-                })
-                .filter(Boolean);
+                }).filter(Boolean);
             }
-
             return {
                 name: folder,
                 files: resourceFiles,
@@ -89,11 +77,12 @@ async function processResources(projectPath:any) {
         }));
         return resources;
         console.log('All Resources:', JSON.stringify(resources, null, 2));
-    } catch (error) {
+    }
+    catch (error) {
         console.error('Error processing resources:', error);
     }
 }
-async function createObjectFile(resourcePath:any, resourceName:any, body:any) {
+async function createObjectFile(resourcePath, resourceName, body) {
     const content = `
 package com.natetrystuff.${resourceName};
 
@@ -112,21 +101,19 @@ public class ${resourceName} {
     private String ${body.propertieName};
 }
 `;
-
     // The filename for the Java class
     const fileName = `${resourceName}.java`;
     // Full path where the file will be written
     const fullPath = path.join(resourcePath, fileName);
-
     try {
         await writeFileSync(fullPath, content, 'utf8');
         console.log(`File created successfully at: ${fullPath}`);
-    } catch (error) {
+    }
+    catch (error) {
         console.error('Error creating Java file:', error);
     }
 }
-
-async function createServiceFile(resourcePath:any, resourceName:any) {
+async function createServiceFile(resourcePath, resourceName) {
     const content = `
 package com.natetrystuff.${resourceName};
 
@@ -168,20 +155,17 @@ public class ${resourceName}Service {
     }
 }
 `;
-
     const fileName = `${resourceName}Service.java`;
     const fullPath = path.join(resourcePath, fileName);
-
     try {
         await writeFileSync(fullPath, content, 'utf8');
         console.log(`Service file created successfully at: ${fullPath}`);
-    } catch (error) {
+    }
+    catch (error) {
         console.error('Error creating service file:', error);
     }
 }
-
-
-async function createControllerFile(resourcePath:any, resourceName:any) {
+async function createControllerFile(resourcePath, resourceName) {
     const content = `
 package com.natetrystuff.${resourceName};
 
@@ -238,40 +222,27 @@ public class ${resourceName}Controller {
     }
 }
 `;
-
     const fileName = `${resourceName}Controller.java`;
     const fullPath = path.join(resourcePath, fileName);
-
     try {
         await writeFileSync(fullPath, content, 'utf8');
         console.log(`Controller file created successfully at: ${fullPath}`);
-    } catch (error) {
+    }
+    catch (error) {
         console.error('Error creating controller file:', error);
     }
 }
-
-async function hasMany(projectPath:string, body:any) {
+async function hasMany(projectPath, body) {
+    console.log(body);
     const class1 = body.class1;
     const class2 = body.class2;
-    const class1Path = `${projectPath}/src/main/java/com/natetrystuff/${class1}/${class1}.java` ;
-    const class2Path = `${projectPath}/src/main/java/com/natetrystuff/${class2}/${class1}.java` ;
-    const lineToAddNewProperty = findLineToAddNewProperty(class1Path);
-    addToFile(class1Path, lineToAddNewProperty, `\tprivate List<${class2}> ${class2.toLowerCase()}s;`);
+    const class1Path = path.join(projectPath, '/src/main/java/com/natetrystuff/', class1);
+    const class2Path = path.join(projectPath, '/src/main/java/com/natetrystuff/', class2);
+    const class1ObjectFile = readFileSync(class1Path + '/' + class1 + '.java', 'utf8');
+    console.log(class1ObjectFile);
+    //first find where to add to object class
 }
-
-const findLineToAddNewProperty = (filePath: string): number => {
-    const fileContent = readFileSync(filePath, 'utf8'); 
-    const lines = fileContent.split('\n'); 
-    for (let i = lines.length - 1; i >= 0; i--) {
-        if (lines[i].trim() === '}') {
-            return i;
-        }
-    }
-
-    return -1;
-};
-
-async function createRepositoryFile(resourcePath:string, resourceName:string) {
+async function createRepositoryFile(resourcePath, resourceName) {
     const content = `
 package com.natetrystuff.${resourceName};
 
@@ -282,28 +253,25 @@ import org.springframework.stereotype.Repository;
 public interface ${resourceName}Repository extends JpaRepository<${resourceName}, Long> {
 }
 `;
-
     const fileName = `${resourceName}Repository.java`;
     const fullPath = path.join(resourcePath, fileName);
-
     try {
         await writeFileSync(fullPath, content, 'utf8');
         console.log(`Repository file created successfully at: ${fullPath}`);
-    } catch (error) {
+    }
+    catch (error) {
         console.error('Error creating repository file:', error);
     }
 }
-
-
-async function createNewResource(projectPath: string, resourceName: string, body:any) {
+async function createNewResource(projectPath, resourceName, body) {
     try {
-        console.log('here')
+        console.log('here');
         const resourcePath = path.join(projectPath, '/src/main/java/com/natetrystuff/', resourceName);
-        
         try {
-            await fs.access(resourcePath);  // Try to access the directory to check if it exists
+            await fs.access(resourcePath); // Try to access the directory to check if it exists
             console.log('Resource folder already exists at:', resourcePath);
-        } catch {
+        }
+        catch (_a) {
             // If the directory does not exist, access will throw an error which we catch here to then create the directory
             await fs.mkdir(resourcePath, { recursive: true });
             console.log('New resource folder created at:', resourcePath);
@@ -312,78 +280,58 @@ async function createNewResource(projectPath: string, resourceName: string, body
             createServiceFile(resourcePath, resourceName);
             createControllerFile(resourcePath, resourceName);
         }
-        
-    } catch (error) {
+    }
+    catch (error) {
         console.error('Error creating new resource folder:', error);
     }
 }
-
-
-interface Options {
-    description: 'all' | 'one';
-  }
-  
-function buildRestCall(method: 'GET' | 'POST' | 'DELETE', resource: string, options: Options, controllerFile:any): void {
-  
+function buildRestCall(method, resource, options, controllerFile) {
     if (method === 'GET' && options.description === 'all') {
-    //   addGetAllRequest(controllerFile, resource);
+        //   addGetAllRequest(controllerFile, resource);
     }
-  
     if (method === 'GET' && options.description === 'one') {
-      addGetOneRequest(controllerFile, resource);
+        addGetOneRequest(controllerFile, resource);
     }
-  
     if (method === 'POST' && options.description === 'one') {
-    //   addPostOneRequest(controllerFile, resource);
+        //   addPostOneRequest(controllerFile, resource);
     }
-  
     if (method === 'DELETE' && options.description === 'one') {
-    //   addDeleteOneRequest(controllerFile, resource);
+        //   addDeleteOneRequest(controllerFile, resource);
     }
-  }
-  
-function addGetOneRequest(controllerFile: string, resource: string): void {
-const MOCK_CODE = `
+}
+function addGetOneRequest(controllerFile, resource) {
+    const MOCK_CODE = `
     @GetMapping("/{id}")
     public ResponseEntity<${resource}> get${resource}ById(@PathVariable Long id) {
     return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 `;
-
-console.log(resource)
-const CODE_READY = MOCK_CODE.replace('resource', resource);
-
-console.log('CODE_READY:', CODE_READY);
-
-console.log('Controller File:', controllerFile)
-const lineToAdd = findLineToAddGetOneRequest(controllerFile);
-console.log('Line to add:', lineToAdd);
-
-addToFile(`${projectPath}+${controllerFile}`, lineToAdd, CODE_READY);
-
-
+    console.log(resource);
+    const CODE_READY = MOCK_CODE.replace('resource', resource);
+    console.log('CODE_READY:', CODE_READY);
+    console.log('Controller File:', controllerFile);
+    const lineToAdd = findLineToAddGetOneRequest(controllerFile);
+    console.log('Line to add:', lineToAdd);
+    addToFile(controllerFile, lineToAdd, CODE_READY);
 }
-
-function findLineToAddGetOneRequest(filePath: string): number {
-  const fileContent = readFileSync(projectPath+filePath, 'utf8');
-  const lines = fileContent.split('\n');
-  const pattern = '@PostMapping';
-
-  for (let i = 0; i < lines.length; i++) {
-    if (lines[i].includes(pattern)) {
-      return i; 
-    }
-  }
-  return -1;
-}
-
-function addToFile(filePath: string, line: number, text: string): void {
-    const fileContent = readFileSync(filePath, 'utf8');
+function findLineToAddGetOneRequest(filePath) {
+    const fileContent = readFileSync(projectPath + filePath, 'utf8');
     const lines = fileContent.split('\n');
-      lines.splice(line - 1, 0, text);
-  
+    const pattern = '@PostMapping';
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i].includes(pattern)) {
+            return i;
+        }
+    }
+    return -1;
+}
+function addToFile(filePath, line, text) {
+    const fileContent = readFileSync(projectPath + filePath, 'utf8');
+    const lines = fileContent.split('\n');
+    // Adjust for 0-indexed array; insert the new content just before the specified line
+    lines.splice(line - 1, 0, text);
+    // Join the array back into a single string with new lines and write it back to the file
     const updatedContent = lines.join('\n');
-    writeFileSync(filePath, updatedContent, 'utf8');
-  }
-
-  const projectPath = '/Users/nathanpieraut/projects/natetrystuff-api/natetrystuff';
+    writeFileSync(projectPath + filePath, updatedContent, 'utf8');
+}
+const projectPath = '/Users/nathanpieraut/projects/natetrystuff-api/natetrystuff';
